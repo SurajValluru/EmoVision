9781e7a86befa3e0be22b9228ac66c3e1967e87b
DeadPooL <80475876+SurajValluru@users.noreply.github.com>
2022-05-28 08:11:22 +0530

    Fixed: Page refreshes after submitting custom code
    Fixes. Added few constraints for text field in config.

--- static/scripts/config.js
+++ static/scripts/config.js
@@ -1,28 +1,29 @@
 function set_code() {
-  const code_txt = document.getElementById("code").value;
+  const code_txt = code.value;
   const emotion_txt = document.getElementById("emotion").value;
+  code.value = "";
   //   console.log(code_txt.value.charAt(2));
   for (i = 0; i < code_txt.length; i++)
-    switch(code_txt.charAt(i)){
-        case '.':
-        case '-':
-        case ' ':
-            Cookies.set(emotion_txt, code_txt);
-            break;
-        default:
-            window.alert("Invalid Input");
-            return;
+    switch (code_txt.charAt(i)) {
+      case ".":
+      case "-":
+      case " ":
+        Cookies.set(emotion_txt, code_txt);
+        break;
+      default:
+        window.alert("Invalid Input");
+        return;
     }
 }
 
-function set_lang(){
+function set_lang() {
   const lang = document.getElementById("lang").value;
-  Cookies.set("lang",lang)
+  Cookies.set("lang", lang);
 }
 
-function set_duration(){
+function set_duration() {
   const duration = document.getElementById("duration").value;
-  Cookies.set("duration_unit",duration);
+  Cookies.set("duration_unit", duration);
 }
 
 const active = document.querySelector("#custom_config");
@@ -42,6 +43,14 @@ let CUSTOM_DICT = {
 
 let CUSTOM_KEYS = Object.keys(CUSTOM_DICT);
 
+const code = document.getElementById("code");
+code.addEventListener("keyup", function (event) {
+  if (event.key == "Enter") {
+    event.preventDefault();
+    document.getElementById("code_submit").click();
+    code.blur();
+  }
+});
+
 custom = Cookies.get("custom");
-if (custom == undefined)
-      default_all();
+if (custom == undefined) default_all();
